aee82018b22b3460fd8f2f6b5ecb4a0d5614164d
skinisbizapps <skini.biz.apps@icloud.com>
2016-01-16 19:28:14 -0800

    setting up baseline

---
+++ src/main/java/apps/biz/skini/sbrps/SpringbootRatpackSampleApplication.java
@@ -0,0 +1,12 @@
+package apps.biz.skini.sbrps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRatpackSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRatpackSampleApplication.class, args);
+	}
+}
